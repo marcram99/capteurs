fab64ce505a61387b5ab0085e55362da95a25c22
marcram99 <marcram99@gmail.com>
2019-01-17 07:17:59 +0100

    correct class test

--- class_test.py
+++ class_test.py
@@ -3,16 +3,23 @@ class Essai():
     def __init__(self,*args):
         self.no1 = args[0]
         self.no2 = None
-        if len(args) > 1:
+        if len(args) == 2:
             self.no2 = args[1]
-    def fnct1(self):
-        print(self.no1)
+
+    def fnct1(self, *args):
+        if args:
+            print(args[0])
+        else:
+            print(self.no1)
 
 
-    def fnct2(self):
-        print(self.no2)
+    def fnct2(self, *args):
+        if args:
+            print(args[0])
+        else:
+            print(self.no2)
 
 if __name__ == '__main__':
     test = Essai('a','c')
     test.fnct1()
-    test.fnct2('b')
+    test.fnct2()
